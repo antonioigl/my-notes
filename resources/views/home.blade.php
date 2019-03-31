bfc8c54913826e8c7dbdaebfba1db0e55bac5c5d
Antonio <antonio241710@gmail.com>
2019-03-31 11:23:06 +0200

    fix view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Notes: </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,30 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form action="">
+                        <div class="form-group">
+                            <label for="note">Write your note: </label>
+                            <input type="text" class="form-control" name="note">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            Send note
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+            <br>
+
+            <div class="card">
+                <div class="card-header">Publicado en: </div>
+
+                <div class="card-body">
+                    <p>asphjeptjhqpe</p>
+                </div>
+
+                <div class="card-footer">
+                    <button class="btn btn-default">Edit</button>
+                    <button class="btn btn-danger">Remove</button>
                 </div>
             </div>
         </div>
