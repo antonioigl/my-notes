c10101e8ae81e1ebe120cb385c4f518a9921f920
Antonio <antonio241710@gmail.com>
2019-03-31 11:55:05 +0200

    add FormComponent

---
+++ resources/js/components/FormComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <div class="card-header">Notes: </div>
+
+        <div class="card-body">
+            <form action="">
+                <div class="form-group">
+                    <label for="note">Write your note: </label>
+                    <input type="text" class="form-control" name="note">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    Send note
+                </button>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,32 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Notes: </div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form action="">
-                        <div class="form-group">
-                            <label for="note">Write your note: </label>
-                            <input type="text" class="form-control" name="note">
-                        </div>
-                        <button type="submit" class="btn btn-primary">
-                            Send note
-                        </button>
-                    </form>
-                </div>
-            </div>
-
+            <form-component> </form-component>
             <br>
-
             <note-component> </note-component>
-
         </div>
     </div>
 </div>
